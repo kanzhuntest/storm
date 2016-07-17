76e1af8f4dfbbc5f3cca1d19c367c58932495bb9
kanzhuntest <xq_usc@126.com>
2016-07-17 22:18:12 +0800

    1

--- src/main/java/com/usc/helios/App.java
+++ src/main/java/com/usc/helios/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("this is git test!");
+        System.out.println("this is git test!!");
     }
 }
