3393b63d3009d5467a93955443946f2704f0bfb2
xuqiang <xq_usc@126.com>
2016-07-17 22:19:06 +0800

    three commit

--- src/main/java/com/usc/helios/App.java
+++ src/main/java/com/usc/helios/App.java
@@ -8,7 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	System.out.println("this is git test!");
+        //System.out.println( "Hello World!" );
         System.out.println("this is git test!");
     }
 }
